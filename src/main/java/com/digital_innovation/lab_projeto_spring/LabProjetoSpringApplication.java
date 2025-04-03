3daf48b0932684dd30435e15376af5d1c463b75b
Peterson <pettm20@gmail.com>
2025-04-03 08:49:58 -0300

    first commit

---
+++ src/main/java/com/digital_innovation/lab_projeto_spring/LabProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.digital_innovation.lab_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital_innovation/lab_projeto_spring/LabProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_innovation.lab_projeto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
